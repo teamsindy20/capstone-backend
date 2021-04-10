7cbd0a704876760889d86c4a24082b783da52136
Taeuk Gwak <gwak2837@kakao.com>
2021-04-11 00:52:56 +0900

    Fix importing file error in build

--- src/graphql/schema.ts
+++ src/graphql/schema.ts
@@ -1,11 +1,11 @@
 import { loadFilesSync } from '@graphql-tools/load-files'
 import { mergeTypeDefs, mergeResolvers } from '@graphql-tools/merge'
 import { makeExecutableSchema } from '@graphql-tools/schema'
-import path from 'path'
+import { join } from 'path'
 
 // loadFilesSync 실행 시 *.resolver.* 파일에서 모듈을 절대 경로로 불러오면 오류 발생, 꼭 상대 경로로 모듈을 불러와야 함
-const typeDefsArray = loadFilesSync(path.join(__dirname, '**/*.graphql'))
-const resolversArray = loadFilesSync(path.join(__dirname, '**/*.resolver.ts'))
+const typeDefsArray = loadFilesSync(join(__dirname, '**/*.graphql'))
+const resolversArray = loadFilesSync(join(__dirname, '**/*.resolver.*'))
 
 const typeDefs = mergeTypeDefs(typeDefsArray)
 const resolvers = mergeResolvers(resolversArray)
