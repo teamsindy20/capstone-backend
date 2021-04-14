ffae6aca6749f2bd913f225acc694d06b1de2ad2
Taeuk Gwak <gwak2837@kakao.com>
2021-04-14 21:25:29 +0900

    Create graphql objects and queries

---
+++ src/graphql/queries/query.resolver.ts
@@ -0,0 +1,26 @@
+import { QueryResolvers } from 'src/graphql/generated/graphql'
+import { pool } from '../../database/postgres'
+
+const sql = 'select * from users where id=$1;'
+const values = [1]
+
+export const Query: QueryResolvers = {
+  users: async () => {
+    const result = await pool.query('SELECT NOW() as now')
+
+    return [
+      {
+        id: '0',
+        creationDate: result.rows[0].now,
+        email: 'email',
+        token: 'token',
+        name: 'name',
+        age: 12,
+      },
+    ]
+  },
+
+  me: async () => {
+    return null
+  },
+}
