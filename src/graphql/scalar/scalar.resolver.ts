a44994a439436ca01f8f512af333eaeddcca0a03
Taeuk Gwak <gwak2837@kakao.com>
2021-04-18 18:22:33 +0900

    Add graphql custom scalar type

--- src/graphql/scalar/scalar.resolver.ts
+++ src/graphql/scalar/scalar.resolver.ts
@@ -1,3 +1,6 @@
-import { DateTimeResolver } from 'graphql-scalars'
+import { DateTimeResolver, EmailAddressResolver, JWTResolver, URLResolver } from 'graphql-scalars'
 
 export const DateTime = DateTimeResolver
+export const EmailAddress = EmailAddressResolver
+export const JWT = JWTResolver
+export const URL = URLResolver
