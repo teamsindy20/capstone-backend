eaa42a11d6b8f81bb6434cb4cda07bfcc7110417
Taeuk Gwak <gwak2837@kakao.com>
2021-05-02 23:26:46 +0900

    Add authorization on creating post

--- src/graphql/post/Mutation.resolver.ts
+++ src/graphql/post/Mutation.resolver.ts
@@ -1,12 +1,18 @@
+import { ForbiddenError } from 'apollo-server'
 import { pool } from '../../database/postgres'
 import { importSQL } from '../../utils/commons'
 import { MutationResolvers } from '../generated/graphql'
 
 const createPostSQL = importSQL(__dirname, 'sql/createPost.sql')
+const userStoreSQL = importSQL(__dirname, 'sql/userStore.sql')
 
 export const Mutation: MutationResolvers = {
   createPost: async (_, { input }, { user }) => {
-    // if (user.role !== store) throw new AuthenticationError('매장 사장님만 글을 작성할 수 있습니다.')
+    // SQL 하나로 합치기?
+    const { rows: userStoreRows } = await pool.query(await userStoreSQL, [user.id])
+
+    if (!userStoreRows.includes(input.storeId))
+      throw new ForbiddenError('매장 사장님만 글을 작성할 수 있습니다.')
 
     const { rows } = await pool.query(await createPostSQL, [
       input.content,
