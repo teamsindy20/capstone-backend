da34f0fc229eb7343a4786df5174c144279db335
Taeuk Gwak <gwak2837@kakao.com>
2021-05-03 17:26:50 +0900

    Remove useless package

--- src/index.ts
+++ src/index.ts
@@ -4,7 +4,6 @@ import dotenv from 'dotenv'
 
 dotenv.config()
 
-import ON_DEATH from 'death'
 import { server } from './apollo/server'
 import { connectDatabase } from './database/postgres'
 
@@ -14,7 +13,3 @@ server
   .listen({ port: process.env.PORT || 4000 })
   .then(({ url }) => console.log(`🚀  Server ready at ${url}`))
   .catch((reason) => console.error(reason))
-
-ON_DEATH(() => {
-  console.log('Bye bye~')
-})
