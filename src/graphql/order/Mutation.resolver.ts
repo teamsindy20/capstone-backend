85dbff9842d94b1b3e4536e384b680d10996bea8
Taeuk Gwak <gwak2837@kakao.com>
2021-05-07 18:18:41 +0900

    WIP: Add createOrder mutation

--- src/graphql/order/Mutation.resolver.ts
+++ src/graphql/order/Mutation.resolver.ts
@@ -9,6 +9,10 @@ export const Mutation: MutationResolvers = {
   createOrder: async (_, { input }, { user }) => {
     if (!user) throw new AuthenticationError('로그인되어 있지 않습니다. 로그인 후 시도해주세요.')
 
+    // 주어진 메뉴 옵션 목록이 주어진 메뉴 목록에 속하는지 확인
+    // 메뉴 옵션은 최대 1개만 선택 가능
+    // 메뉴 선택 개수는 배열에 선택 개수만큼 중복으로 넣어주기
+
     const { rows } = await pool.query(await createOrderSQL, [
       // input.orderTotal,
       // user.id,
