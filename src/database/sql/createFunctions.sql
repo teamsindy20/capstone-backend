3d0be06fd73a1a6d36b239b4ad9070e7f73dc1dc
Taeuk Gwak <gwak2837@kakao.com>
2021-05-07 18:17:04 +0900

    Modify create_order function

--- src/database/sql/createFunctions.sql
+++ src/database/sql/createFunctions.sql
@@ -6,30 +6,38 @@ CREATE OR REPLACE FUNCTION create_store (
     user_id bigint,
     review_event_content text DEFAULT NULL,
     regular_customer_event_content text DEFAULT NULL,
-    delivery_time_min int DEFAULT NULL,
-    delivery_time_max int DEFAULT NULL,
-    image_urls text [] DEFAULT NULL,
-    hashtags text [] DEFAULT NULL,
+    minimum_delivery_time int DEFAULT NULL,
+    maximum_delivery_time int DEFAULT NULL,
+    image_urls text ARRAY DEFAULT NULL,
+    hashtags text ARRAY DEFAULT NULL,
     out inserted_store_id bigint
   ) language SQL AS $$ WITH inserted_store AS (
     INSERT INTO store (
         name,
         address,
+        business_registration_name,
+        business_registration_number,
+        business_registration_address,
+        business_representative_name,
         user_id,
         review_event_content,
         regular_customer_event_content,
-        delivery_time_min,
-        delivery_time_max,
+        minimum_delivery_time,
+        maximum_delivery_time,
         image_urls
       )
     VALUES (
         name,
         address,
+        '상호명',
+        '1502315983',
+        '사업장소재지',
+        '대표자',
         user_id,
         review_event_content,
         regular_customer_event_content,
-        delivery_time_min,
-        delivery_time_max,
+        minimum_delivery_time,
+        maximum_delivery_time,
         image_urls
       )
     RETURNING id
@@ -91,8 +99,8 @@ CREATE OR REPLACE FUNCTION create_menu (
     price int,
     category_name text,
     store_id bigint,
-    image_urls text [] DEFAULT NULL,
-    hashtags text [] DEFAULT NULL,
+    image_urls text ARRAY DEFAULT NULL,
+    hashtags text ARRAY DEFAULT NULL,
     out inserted_menu_id bigint
   ) language SQL AS $$ WITH inserted_menu AS (
     INSERT INTO menu (name, price, category_id, store_id, image_urls)
@@ -134,32 +142,116 @@ FROM inserted_menu;
 
 $$;
 
+DROP FUNCTION IF EXISTS get_total_price_from;
+
+CREATE OR REPLACE FUNCTION get_total_price_from(
+    menu_ids bigint ARRAY,
+    menu_option_ids bigint ARRAY DEFAULT NULL,
+    out total_price int
+  ) language SQL IMMUTABLE AS $$
+SELECT sum(sum)::int
+FROM(
+    SELECT sum(price)
+    FROM menu
+      JOIN (
+        SELECT unnest(menu_ids) AS id
+      ) AS selected_menu ON menu.id = selected_menu.id
+    UNION ALL
+    SELECT sum(price)
+    FROM menu_option
+      JOIN (
+        SELECT unnest(menu_option_ids) AS id
+      ) AS selected_menu_option ON menu_option.id = selected_menu_option.id
+  ) AS total_selection;
+
+$$;
+
+DROP FUNCTION IF EXISTS get_delivery_charge_from;
+
+CREATE OR REPLACE FUNCTION get_delivery_charge_from(
+    store_id bigint,
+    out delivery_charge int
+  ) language SQL IMMUTABLE AS $$
+SELECT delivery_charge
+FROM store
+WHERE id = store_id;
+
+$$;
+
 DROP FUNCTION IF EXISTS create_order;
 
 CREATE OR REPLACE FUNCTION create_order (
+    delivery_address varchar(64),
+    payment_date timestamptz,
     user_id bigint,
+    payment_id bigint,
     store_id bigint,
-    menu_id_array bigint [],
+    menu_ids bigint ARRAY,
+    menu_option_ids bigint ARRAY DEFAULT NULL,
+    point_used int DEFAULT NULL,
     review_reward boolean DEFAULT NULL,
     regular_reward boolean DEFAULT NULL,
+    delivery_request varchar(256) DEFAULT NULL,
+    coupon_id bigint DEFAULT NULL,
+    store_request varchar(256) DEFAULT NULL,
     out inserted_order_id bigint
-  ) language SQL AS $$ WITH inserted_order AS (
+  ) language SQL AS $$ WITH selected_menu (id) AS (
+    SELECT unnest(menu_ids)
+  ),
+  selected_menu_option (id) AS (
+    SELECT unnest(menu_option_ids)
+  ),
+  inserted_order AS (
     INSERT INTO "order" (
+        delivery_address,
+        menu_total,
+        delivery_charge,
+        payment_date,
         user_id,
-        store_id
+        payment_id,
+        store_id,
+        point_used,
+        review_reward,
+        regular_reward,
+        delivery_request,
+        store_request,
+        coupon_id
       )
-    VALUES (user_id, store_id)
+    VALUES (
+        delivery_address,
+        get_total_price_from(menu_ids, menu_option_ids),
+        get_delivery_charge_from(store_id),
+        payment_date,
+        user_id,
+        payment_id,
+        store_id,
+        point_used,
+        review_reward,
+        regular_reward,
+        delivery_request,
+        store_request,
+        coupon_id
+      )
     RETURNING id
   ),
-  menu_ids (id) AS (
-    SELECT unnest(menu_id_array)
+  update_user_point AS (
+    UPDATE "user"
+    SET point = point - point_used
+    WHERE id = user_id
   ),
-  inserted_menu_x_order AS (
+  inserted_menu_x_order AS(
     INSERT INTO menu_x_order (menu_id, order_id)
     SELECT inserted_order.id,
-      menu_ids.id
+      selected_menu.id
     FROM inserted_order,
-      menu_ids
+      selected_menu
+  ),
+  inserted_menu_option_x_order AS (
+    INSERT INTO menu_option_x_order (menu_option_id, order_id)
+    SELECT inserted_order.id,
+      selected_menu_option.id
+    FROM inserted_order,
+      selected_menu_option
   )
 SELECT id
 FROM inserted_order;
--- src/database/sql/createTables.sql
+++ src/database/sql/createTables.sql
@@ -5,19 +5,20 @@ CREATE TABLE "user" (
   id bigint PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
   creation_date timestamptz NOT NULL DEFAULT NOW(),
   modification_date timestamptz NOT NULL DEFAULT NOW(),
-  valid_authentication_date timestamptz NOT NULL DEFAULT NOW(),
-  point int NOT NULL DEFAULT 0,
-  is_unregistered boolean NOT NULL DEFAULT FALSE,
   --
   email varchar(64) NOT NULL UNIQUE,
   password_hash_hash varchar(128) NOT NULL,
   --
   name varchar(64),
-  phone_number varchar(16),
+  phone_number varchar(32),
   gender varchar(16),
   birth_date timestamptz,
   image_urls text ARRAY,
-  delivery_addresses varchar(64) ARRAY
+  delivery_addresses varchar(64) ARRAY,
+  point int NOT NULL DEFAULT 0 CHECK (point >= 0),
+  --
+  valid_authentication_date timestamptz NOT NULL DEFAULT NOW(),
+  is_unregistered boolean NOT NULL DEFAULT FALSE
 );
 
 DROP TABLE IF EXISTS store CASCADE;
@@ -32,8 +33,17 @@ CREATE TABLE store (
   business_registration_number varchar(64) NOT NULL,
   business_registration_address varchar(64) NOT NULL,
   business_representative_name varchar(64) NOT NULL,
-  delivery_fee int NOT NULL DEFAULT 0,
+  --
+  user_id bigint NOT NULL REFERENCES "user" ON DELETE CASCADE,
+  --
+  delivery_charge int NOT NULL DEFAULT 0,
   minimum_delivery_amount int NOT NULL DEFAULT 0,
+  minimum_delivery_time int,
+  maximum_delivery_time int,
+  image_urls text ARRAY,
+  review_event_content text,
+  regular_customer_event_content text,
+  --
   delicious_review_count int NOT NULL DEFAULT 0,
   fine_review_count int NOT NULL DEFAULT 0,
   bad_review_count int NOT NULL DEFAULT 0,
@@ -43,15 +53,7 @@ CREATE TABLE store (
   regular_customer_count int NOT NULL DEFAULT 0,
   favorite_count int NOT NULL DEFAULT 0,
   click_count int NOT NULL DEFAULT 0,
-  post_count int NOT NULL DEFAULT 0,
-  --
-  user_id bigint NOT NULL REFERENCES "user" ON DELETE CASCADE,
-  --
-  review_event_content text,
-  regular_customer_event_content text,
-  delivery_time_min int,
-  delivery_time_max int,
-  image_urls text ARRAY
+  post_count int NOT NULL DEFAULT 0
 );
 
 DROP TABLE IF EXISTS menu_category CASCADE;
@@ -79,6 +81,14 @@ CREATE TABLE menu (
   id bigint PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
   creation_date timestamptz NOT NULL DEFAULT NOW(),
   modification_date timestamptz NOT NULL DEFAULT NOW(),
+  --
+  name varchar(64) NOT NULL,
+  price int NOT NULL,
+  --
+  store_id bigint NOT NULL REFERENCES store ON DELETE CASCADE,
+  category_id bigint NOT NULL REFERENCES menu_category ON DELETE CASCADE,
+  --
+  image_urls text ARRAY,
   delicious_review_count int NOT NULL DEFAULT 0,
   fine_review_count int NOT NULL DEFAULT 0,
   bad_review_count int NOT NULL DEFAULT 0,
@@ -92,14 +102,6 @@ CREATE TABLE menu (
   is_discounted boolean NOT NULL DEFAULT FALSE,
   can_be_picked boolean NOT NULL DEFAULT FALSE,
   can_be_reserved boolean NOT NULL DEFAULT FALSE,
-  --
-  name varchar(64) NOT NULL,
-  price int NOT NULL,
-  --
-  store_id bigint NOT NULL REFERENCES store ON DELETE CASCADE,
-  category_id bigint NOT NULL REFERENCES menu_category ON DELETE CASCADE,
-  --
-  image_urls text ARRAY,
   --
   theme_id bigint REFERENCES menu_theme ON DELETE CASCADE
 );
@@ -119,12 +121,13 @@ CREATE INDEX menu_theme_id ON menu (theme_id);
 DROP TABLE IF EXISTS menu_option_category CASCADE;
 
 -- type은 '양자택일형', '단일선택형', '다중선택형', '서술형'
+-- selection_count는 '다중선택형'일 때 사용
 CREATE TABLE menu_option_category (
   id bigint PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
   creation_date timestamptz NOT NULL DEFAULT NOW(),
   modification_date timestamptz NOT NULL DEFAULT NOW(),
   is_necessary boolean NOT NULL DEFAULT false,
-  --
+  --  
   name varchar(32) NOT NULL,
   "type" varchar(16) NOT NULL,
   --
@@ -164,45 +167,42 @@ CREATE TABLE "order" (
   modification_date timestamptz NOT NULL DEFAULT NOW(),
   --
   delivery_address varchar(64) NOT NULL,
-  order_status varchar(16) NOT NULL DEFAULT '접수 대기',
-  review_reward boolean NOT NULL DEFAULT false,
-  regular_reward boolean NOT NULL DEFAULT false,
-  menu_total int NOT NULL,
-  point_used int NOT NULL,
-  delivery_charge int NOT NULL,
+  menu_total int NOT NULL CHECK (menu_total >= 0),
+  delivery_charge int NOT NULL CHECK (delivery_charge >= 0),
   payment_date timestamptz NOT NULL,
-  --
-  store_name varchar(64) NOT NULL,
-  store_address varchar(64) NOT NULL,
-  store_business_registration_name varchar(64) NOT NULL,
-  store_business_registration_number varchar(64) NOT NULL,
-  store_business_registration_address varchar(64) NOT NULL,
-  store_business_representative_name varchar(64) NOT NULL,
   --
   user_id bigint NOT NULL REFERENCES "user" ON DELETE CASCADE,
   payment_id bigint NOT NULL REFERENCES payment ON DELETE CASCADE,
+  store_id bigint NOT NULL REFERENCES store ON DELETE CASCADE,
   --
+  order_status varchar(16) NOT NULL DEFAULT '접수 대기',
+  point_used int NOT NULL DEFAULT 0 CHECK (point_used >= 0),
+  review_reward boolean NOT NULL DEFAULT false,
+  regular_reward boolean NOT NULL DEFAULT false,
   delivery_request varchar(256),
-  store_request varchar(256)
+  store_request varchar(256),
+  --
+  coupon_id bigint NOT NULL REFERENCES coupon ON DELETE CASCADE
 );
 
 DROP TABLE IF EXISTS coupon CASCADE;
 
 -- type은 '정액형', '정률형'
--- user_id 또는 store_id 가 존재해야 한다. 둘 다 없으면 안 된다.
+-- user_id: 쿠폰 소유 사용자 
+-- store_id: 쿠폰 발행 매장
+-- order_id: 쿠폰을 사용한 주문
 -- 쿠폰의 유효기간 = 쿠폰 다운로드 가능 기간
 CREATE TABLE coupon (
   id bigint PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
   creation_date timestamptz NOT NULL DEFAULT NOW(),
-  modification_date timestamptz NOT NULL DEFAULT NOW(),
   --
   name varchar(32) NOT NULL,
   "type" varchar(16) NOT NULL,
-  discount_amount int NOT NULL,
-  minimum_order_amount int NOT NULL,
+  discount_amount int NOT NULL CHECK (discount_amount >= 0),
+  minimum_order_amount int NOT NULL CHECK (minimum_order_amount >= 0),
   is_used boolean NOT NULL DEFAULT false,
   expiration_start_date timestamptz NOT NULL,
-  expiration_end_date timestamptz NOT NULL,
+  expiration_end_date timestamptz NOT NULL CHECK (expiration_start_date < expiration_end_date),
   --
   store_id bigint REFERENCES store ON DELETE CASCADE,
   user_id bigint REFERENCES "user" ON DELETE CASCADE,
@@ -221,19 +221,20 @@ CREATE TABLE store_x_coupon (
   PRIMARY KEY (store_id, coupon_id)
 );
 
-DROP TABLE IF EXISTS order_x_menu_option;
+DROP TABLE IF EXISTS order_x_menu;
 
 -- 주문할 때 선택한 메뉴 옵션
 -- menu_option_text는 메뉴 옵션이 '서술형'일 떄 사용
-CREATE TABLE order_x_menu_option (
+CREATE TABLE order_x_menu (
   order_id bigint REFERENCES "order" ON DELETE CASCADE,
-  menu_option_id bigint REFERENCES menu_option ON DELETE CASCADE,
+  menu_id bigint REFERENCES menu ON DELETE CASCADE,
   creation_date timestamptz NOT NULL DEFAULT NOW(),
   modification_date timestamptz NOT NULL DEFAULT NOW(),
   --
-  menu_option_text text,
+  "count" int NOT NULL,
+  menu_option_selections jsonb,
   --
-  PRIMARY KEY (store_id, coupon_id)
+  PRIMARY KEY (order_id, menu_id)
 );
 
 -- DROP TABLE IF EXISTS promotion CASCADE;
@@ -255,7 +256,6 @@ CREATE TABLE review (
   id bigint PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
   creation_date timestamptz NOT NULL DEFAULT NOW(),
   modification_date timestamptz NOT NULL DEFAULT NOW(),
-  helping_others_count int NOT NULL DEFAULT 0,
   --
   rating varchar(16) NOT NULL,
   --
@@ -263,6 +263,7 @@ CREATE TABLE review (
   store_id bigint NOT NULL REFERENCES store ON DELETE CASCADE,
   order_id bigint NOT NULL REFERENCES "order" ON DELETE CASCADE,
   --
+  helping_others_count int NOT NULL DEFAULT 0,
   image_urls text ARRAY,
   good_point_content text,
   desired_point_content text
@@ -388,6 +389,17 @@ CREATE TABLE menu_x_order (
   modification_date timestamptz NOT NULL DEFAULT NOW(),
   --
   PRIMARY KEY (menu_id, order_id)
+);
+
+DROP TABLE IF EXISTS menu_option_x_order;
+
+CREATE TABLE menu_option_x_order (
+  menu_option_id bigint REFERENCES menu_option,
+  order_id bigint REFERENCES "order",
+  creation_date timestamptz NOT NULL DEFAULT NOW(),
+  modification_date timestamptz NOT NULL DEFAULT NOW(),
+  --
+  PRIMARY KEY (menu_option_id, order_id)
 );
 
 DROP TABLE IF EXISTS review_x_hashtag;
