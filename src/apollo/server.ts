765744b7cbc50d60088f6e38c6143864b61cb063
Taeuk Gwak <gwak2837@kakao.com>
2021-05-08 15:23:25 +0900

    Move user sql

--- src/apollo/server.ts
+++ src/apollo/server.ts
@@ -5,7 +5,7 @@ import { pool } from '../database/postgres'
 import { verifyJWT } from '../utils/jwt'
 import { importSQL } from '../utils/commons'
 
-const userSQL = importSQL(__dirname, 'user.sql')
+const user = importSQL(__dirname, '/../graphql/user/sql/user.sql')
 
 export const server = new ApolloServer({
   context: async ({ req }) => {
@@ -17,10 +17,7 @@ export const server = new ApolloServer({
 
     if (!result) return { user: null } // JWT가 없거나, JWT 서명이 유효하지 않거나, JWT 유효기간이 만료됐을 때
 
-    const { rowCount, rows } = await pool.query(await userSQL, [
-      result.userId,
-      result.lastLoginDate,
-    ])
+    const { rowCount, rows } = await pool.query(await user, [result.userId, result.lastLoginDate])
 
     if (!rowCount) return { user: null } // 로그아웃 등으로 인해 JWT가 유효하지 않을 때
 
