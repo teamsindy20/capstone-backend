7806dcfd02cc8e3890fc7d00f9b286ac0d77d9fe
Taeuk Gwak <gwak2837@kakao.com>
2021-05-09 17:38:30 +0900

    Fix querying store of the post

--- src/graphql/post/Object.resolver.ts
+++ src/graphql/post/Object.resolver.ts
@@ -1,6 +1,7 @@
 import { PostResolvers } from 'src/graphql/generated/graphql'
 import { importSQL } from '../../utils/commons'
 import { pool } from '../../database/postgres'
+import { storeORM } from '../store/ORM'
 
 const postStore = importSQL(__dirname, 'sql/postStore.sql')
 const postHashtag = importSQL(__dirname, 'sql/postHashtag.sql')
@@ -9,7 +10,7 @@ export const Post: PostResolvers = {
   store: async ({ storeId }) => {
     const { rows } = await pool.query(await postStore, [storeId])
 
-    return rows[0]
+    return storeORM(rows[0])
   },
 
   hashtags: async ({ id }) => {
