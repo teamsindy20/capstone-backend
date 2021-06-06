2d4108118ef4371d1ecb56435027ea158c06c388
Taeuk Gwak <gwak2837@kakao.com>
2021-06-07 02:11:23 +0900

    Add querying order

--- src/graphql/order/Query.resolver.ts
+++ src/graphql/order/Query.resolver.ts
@@ -3,23 +3,28 @@ import format from 'pg-format'
 import { importSQL } from '../../utils/commons'
 import { poolQuery } from '../../database/postgres'
 import { QueryResolvers } from '../generated/graphql'
-import { orderORM } from './ORM'
+import { orderFieldColumnMapping, orderORM } from './ORM'
+import { selectColumnFromField } from '../../utils/ORM'
 
 const orders = importSQL(__dirname, 'sql/orders.sql')
 
 export const Query: QueryResolvers = {
-  orders: async (_, __, { user }) => {
+  orders: async (_, __, { user }, info) => {
     if (!user) throw new AuthenticationError('로그인되어 있지 않습니다. 로그인 후 시도해주세요.')
 
-    const { rows } = await poolQuery(await orders, [user.id])
+    const columns = selectColumnFromField(info, orderFieldColumnMapping)
+
+    const { rows } = await poolQuery(format(await orders, columns), [user.id])
 
     return rows.map((row) => orderORM(row))
   },
 
-  order: async (_, { id }, { user }) => {
+  order: async (_, { id }, { user }, info) => {
     if (!user) throw new AuthenticationError('로그인되어 있지 않습니다. 로그인 후 시도해주세요.')
 
-    const { rows } = await poolQuery(await orders, [id])
+    const columns = selectColumnFromField(info, orderFieldColumnMapping)
+
+    const { rows } = await poolQuery(format(await orders, columns), [id])
 
     return orderORM(rows[0])
   },
